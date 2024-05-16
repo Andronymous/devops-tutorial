9316626c854507eac7c9ba00dd1c6e5d2aad3b86
Andronymous <andronystudio@gmail.com>
2024-05-16 10:20:43 +0330

    fix and relocate debugging samples

--- bash-scripting/debugging/set-e.sh
+++ bash-scripting/debugging/set-e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 non_existent_command
-echo "This won't be printed“
+echo "This won't be printed"
 set +e
--- bash-scripting/debugging/set-x.sh
+++ bash-scripting/debugging/set-x.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-echo "Hello, World!“
+echo "Hello, World!"
 set +x
